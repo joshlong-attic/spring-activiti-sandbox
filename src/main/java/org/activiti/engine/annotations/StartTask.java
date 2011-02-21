9497afb2f3c4594fb68b00b83f6e9d8f61079bfe
Josh Long <josh@joshlong.com>
2011-02-21 06:21:28 -0800

    clean up..

--- src/main/java/org/activiti/engine/annotations/StartTask.java
+++ src/main/java/org/activiti/engine/annotations/StartTask.java
@@ -7,4 +7,11 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface StartTask {
+
+	
+	/**
+	 * the name of the task to start work on
+	 * @return the name of the task
+	 */
+	String value()   ;
 }
--- src/test/java/org/activiti/spring/test/components/ProcessInitiatingPojo.java
+++ src/test/java/org/activiti/spring/test/components/ProcessInitiatingPojo.java
@@ -2,47 +2,47 @@ package org.activiti.spring.test.components;
 
 
 import org.activiti.engine.annotations.BusinessKey;
-import org.activiti.engine.annotations.ProcessId;
 import org.activiti.engine.annotations.ProcessVariable;
 import org.activiti.engine.annotations.StartProcess;
 import org.activiti.engine.runtime.ProcessInstance;
 
 import java.util.logging.Logger;
 
+/**
+ * simple class that demonstrates the annotations to implicitly handle annotation-driven process managment
+ *
+ * @author Josh Long
+ * @since 5.3
+ */
 public class ProcessInitiatingPojo {
 
 	private Logger log = Logger.getLogger(getClass().getName());
 
 	private int methodState = 0;
 
-	public void reset(){
-		this.methodState=0;
+	public void reset() {
+		this.methodState = 0;
 	}
-
-	/*
-	public void log (ActivityExecution execution){
-		 log.info("execution:" + ToStringBuilder.reflectionToString(execution ) ) ;
-	}*/
 
 	@StartProcess(processKey = "b")
 	public void startProcess(@ProcessVariable("customerId") long customerId) {
-		log.info( "starting 'b' with customerId # " + customerId);
-		this.methodState+=1;
-		log.info ( "up'd the method state");
+		log.info("starting 'b' with customerId # " + customerId);
+		this.methodState += 1;
+		log.info("up'd the method state");
 	}
 
 	public int getMethodState() {
 		return methodState;
 	}
 
-	@StartProcess(processKey = "waiter",returnProcessInstanceId = true )
-	public String startProcessA(@ProcessVariable("customerId" ) long cId){
+	@StartProcess(processKey = "waiter", returnProcessInstanceId = true)
+	public String startProcessA(@ProcessVariable("customerId") long cId) {
 		return null;
 	}
 
 	@StartProcess(processKey = "waiter")
-	public ProcessInstance enrollCustomer( @BusinessKey String key,  @ProcessVariable("customerId") long customerId ){
-		return null  ;
+	public ProcessInstance enrollCustomer(@BusinessKey String key, @ProcessVariable("customerId") long customerId) {
+		return null;
 	}
 
 }
