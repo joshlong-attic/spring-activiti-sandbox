d149c29efcaebc4881cb434cba6a45eefb2efb9d
Josh Long <josh.long@springsource.com>
2011-02-24 21:30:45 -0800

    merging in changes from the module in the latest release.

--- src/main/java/org/activiti/spring/components/scope/ProcessInstanceScopeContextHolder.java
+++ src/main/java/org/activiti/spring/components/scope/ProcessInstanceScopeContextHolder.java
@@ -1,36 +1,42 @@
 package org.activiti.spring.components.scope;
 
+import org.activiti.engine.impl.context.Context;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.springframework.core.NamedThreadLocal;
 
 /**
  * mutex for {@link org.activiti.engine.runtime.ProcessInstance} state.
- *
+ * <p/>
  * todo setup corresponding logic to register the {@link ProcessInstance} and destroy it on {@link ProcessInstance} lifetimes.
- *
  *
  * @author Josh Long
  */
 public abstract class ProcessInstanceScopeContextHolder {
+//
+//	private static final ThreadLocal<ProcessInstance> processInstanceHolder =
+//			new NamedThreadLocal<ProcessInstance>(ProcessInstance.class.getName());
 
-	private static final ThreadLocal<ProcessInstance> processInstanceHolder =
-			new NamedThreadLocal<ProcessInstance>(ProcessInstance.class.getName());
+//	public static void reset() {
+//		// todo 	processInstanceHolder.set(null);
+//
+//	}
 
-	public static void reset() {
-		processInstanceHolder.set(null);
-	}
+		
+	public static void setProcessInstance(ProcessInstance processInstance) {}
+
+	public static void reset() {}
 
 	public static ProcessInstance getCurrentProcessInstance() {
-		ProcessInstance processInstance = processInstanceHolder.get();
-		if(processInstance == null){
-			 throw new IllegalStateException("there is no " +
-					 ProcessInstance.class.getName() + " bound to the current thread!") ;
+
+		ProcessInstance processInstance = Context.getExecutionContext().getProcessInstance();
+
+		if (processInstance == null) {
+			throw new IllegalStateException("there is no " + ProcessInstance.class.getName() + " bound to the current thread!");
 		}
 		return processInstance;
 	}
-
-	public static void setProcessInstance(ProcessInstance processInstance ){
-		processInstanceHolder.set(processInstance);
-	}
-
+//
+//	public static void setProcessInstance(ProcessInstance processInstance) {
+//	//	processInstanceHolder.set(processInstance);
+//	}
 }
